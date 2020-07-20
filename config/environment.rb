6900abb9150177acca45e6686d69b0a052d4e86a
Jan Vacek <honza394@gmail.com>
2020-07-20 21:13:31 +0200

    sendgrid environment settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.photo_6_api_user,
+  :password => Rails.application.credentials.photo_6_api_key,
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
